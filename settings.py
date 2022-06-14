4b47fc3eb4aa4aab3f22796620b475daf3d165b0
toorts <torchester@gmail.com>
2022-06-14 12:25:43 +0500

    change bullet settings

--- settings.py
+++ settings.py
@@ -12,10 +12,10 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 10
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 10
 
         # Alien settings
         self.fleet_drop_speed = 10
